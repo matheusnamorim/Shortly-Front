1c35c1da72b1cfd60f2261838f617e750e3637c9
Amorim <matheusnavesamorim@gmail.com>
2022-10-16 17:51:15 -0300

    Refactor: NavBar

--- src/components/LoggedOut/LoggedOut.js
+++ src/components/LoggedOut/LoggedOut.js
@@ -1,40 +1,16 @@
-import styled from 'styled-components';
 import { useState } from 'react';
+import NavBar from '../../styles/NavBar';
 
 export default function LoggedOut(){
     return (
         <>
             <NavBar>
-                <Entrys>
+                <div>
                     <p>Entrar</p>
                     <h1>Cadastrar-se</h1>
-                </Entrys>
+                </div>
             </NavBar>
         </>
     );
 };
 
-const NavBar = styled.div`
-    height: 100px;
-    width: 100%;
-    padding-right: 40px;
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-`;
-
-const Entrys = styled.div`
-    display: flex;
-    p{
-        margin-right: 25px;
-        font-size: 14px;
-        font-weight: 400;
-        color: #5D9040;
-    }
-    h1{
-        color: #9C9C9C;
-        margin-right: 25px;
-        font-size: 14px;
-        font-weight: 400;
-    }
-`;
---
+++ src/styles/NavBar.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export default function NavBar({children}){
+    return <Wrapper>{children}</Wrapper>
+}
+
+const Wrapper = styled.div`
+    height: 100px;
+    width: 100%;
+    padding-right: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    div{
+    display: flex;
+    }
+    
+    p{
+        margin-right: 25px;
+        font-size: 14px;
+        font-weight: 400;
+        color: #5D9040;
+    }
+    h1{
+        color: #9C9C9C;
+        margin-right: 25px;
+        font-size: 14px;
+        font-weight: 400;
+    }
+`;
