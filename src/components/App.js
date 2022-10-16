5f46a633ca1458794597721e1eff36868f063a01
Amorim <matheusnavesamorim@gmail.com>
2022-10-16 17:18:08 -0300

    Arquivos Iniciais

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import GlobalStyle from '../styles/globalStyles.js';
+
+export default function App(){
+    return (
+        <>
+            <GlobalStyle/>
+            <p>oi</p>
+        </>
+    );
+}
