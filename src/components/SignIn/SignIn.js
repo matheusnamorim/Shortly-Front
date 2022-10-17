844933c4cea9af7a1a881136a3cf785ca80f9671
Amorim <matheusnavesamorim@gmail.com>
2022-10-16 23:12:42 -0300

    Feato: layout rota signin

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,8 +1,29 @@
+import NavBar from "../../styles/NavBar";
+import { Link } from "react-router-dom";
+import Container from "../../styles/Container";
+import { Logo, Form } from "../../styles/styles";
+import logo from '../../assets/Logo.png';
 
 export default function SignIn(){
     return (
       <>
-        <p>ata</p>
+          <NavBar>
+            <div>
+                <Link to='/signin' style={{ textDecoration: 'none' }}><p>Entrar</p></Link>
+                <Link to='/signup' style={{ textDecoration: 'none' }}><h1>Cadastrar-se</h1></Link>
+            </div>
+        </NavBar> 
+        <Container aligner={true}>
+            <Logo>
+                <h1>Shortly</h1>
+                <img src={logo}/>
+            </Logo> 
+            <Form onSubmit={(e) => e.preventDefault()}>
+                <input placeholder='E-mail'/>
+                <input placeholder='Senha'/>
+                <button>Entrar</button>
+            </Form>
+        </Container>
       </>
       );
   };
